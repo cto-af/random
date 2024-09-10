31f6fc2f0f2f277ba90d1ca530b968ab5ce70347
Joe Hildebrand <joe-github@cursive.net>
2024-09-10 14:31:00 -0600

    Fix repo URLs in docs

--- typedoc.config.cjs
+++ typedoc.config.cjs
@@ -6,14 +6,15 @@ module.exports = {
   out: 'docs',
   cleanOutputDir: true,
   sidebarLinks: {
-    GitHub: 'https://github.com/hildjj/foo/',
-    Documentation: 'http://hildjj.github.io/foo/',
+    GitHub: 'https://github.com/cto-af/random',
+    Documentation: 'https://cto-af.github.io/random/',
   },
   navigation: {
     includeCategories: false,
     includeGroups: false,
   },
   categorizeByGroup: false,
+  includeVersion: true,
   sort: ['static-first', 'alphabetical'],
   exclude: ['**/*.spec.ts'],
 };
