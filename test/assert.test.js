2ac345eb08498b5cabb8725274d5b18894ddcd81
Joe Hildebrand <joe-github@cursive.net>
2024-09-11 14:06:43 -0600

    Lint

--- test/assert.test.js
+++ test/assert.test.js
@@ -1,4 +1,5 @@
 import {assert} from '../lib/assert.js';
+// eslint-disable-next-line n/no-unsupported-features/node-builtins
 import {test} from 'node:test';
 import {throws} from 'node:assert/strict';
 
